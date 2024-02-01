e6c339a7b9c6bba51b72017962aa32d87146570d
Landry Houston <landry.houston99@gmail.com>
2024-02-01 11:42:18 -0800

    ASL_translator

---
+++ README.md
@@ -0,0 +1,6 @@
+<h1 align='center'>
+ASL_Translator
+<h3 align='center'>
+Capstone Project repository for General Assembly DSI-1113
+<h3>
+<h1>
---
+++ code/streamlit.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import numpy as np
+from PIL import Image
+from keras.models import load_model
+
+model = load_model("../data/model.h5")
+
+
+# Function to preprocess the input image
+def preprocess_image(image):
+    img = image.convert("L").resize((28, 28))
+    img_array = np.array(img)
+    img_array = img_array / 255.0
+    img_array = img_array.reshape(1, 28, 28, 1)
+    return img_array
+
+
+# Function to convert labels to letters
+def map_label_to_letter(label):
+    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    return letters[label]
+
+
+st.title("Sign Language Recognition App")
+
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    processed_image = preprocess_image(image)
+
+    prediction = model.predict(processed_image)
+    predicted_label = np.argmax(prediction)
+    confidence_percentage = np.max(prediction) * 100
+
+    predicted_letter = map_label_to_letter(predicted_label)
+
+    st.write("Prediction:", predicted_letter)
+    st.write("Confidence:", f"{confidence_percentage:.2f}%")
